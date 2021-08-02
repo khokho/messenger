141cc2417334bf9d322abf43083ee01f3ab4a4f0
Aleksandre Khokhiashvili <skhokhi@gmail.com>
2021-08-02 13:12:47 +0400

    init

---
+++ app/src/main/java/ge/ttopu18alkhok18/messenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package ge.ttopu18alkhok18.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
