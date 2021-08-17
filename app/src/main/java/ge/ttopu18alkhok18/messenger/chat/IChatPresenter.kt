acd29e4ce8c2c9ebada64cc38d4eb9f02b9c3d2e
tamtatop <ttopu18@freeuni.edu.ge>
2021-08-17 15:57:07 +0400

    chat interactor should be done

--- app/src/main/java/ge/ttopu18alkhok18/messenger/chat/IChatPresenter.kt
+++ app/src/main/java/ge/ttopu18alkhok18/messenger/chat/IChatPresenter.kt
@@ -1,8 +1,13 @@
 package ge.ttopu18alkhok18.messenger.chat
 
 import ge.ttopu18alkhok18.messenger.models.Message
+import ge.ttopu18alkhok18.messenger.models.User
 
 interface IChatPresenter {
     fun sendMessage(message: Message)
     fun startListeningToMessages(toUsername: String, chatKey: String)
+
+
+    fun newMessageArrived(message: Message) // could be from me
+    fun userFetched(user: User)
 }
